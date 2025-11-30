9b5f17a9cb53a281f923ea69536748aceb8e1358
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-11-30 18:45:59 -0500

    updated tabs layout

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -35,15 +35,7 @@ export default function TabLayout() {
           ),
         }}
       />
-      {/* <Tabs.Screen
-        name="create"
-        options={{
-          title: "Create",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="add-circle-outline" size={size} color={color} />
-          ),
-        }}
-      />
+      {/* 
       <Tabs.Screen
         name="profile"
         options={{
